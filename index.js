b8acb06afc3f0cf8936aeb0032227fb03c3cbb7f
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-13 16:08:41 -0400

    have seperate initial states and reducer functions

--- index.js
+++ index.js
@@ -17,20 +17,51 @@ function buyIceCream() {
   };
 }
 
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIcecreams: 20,
+//   };
+
 // Initial state
-const initialState = {
+const initialCakeState = {
   numOfCakes: 10,
+};
+
+const initialIceCreamState = {
   numOfIcecreams: 20,
 };
 
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         numOfIcecreams: state.numOfIcecreams - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 // Reducer
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
