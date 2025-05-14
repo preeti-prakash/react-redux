7862721d4e1fb70196a14a9dd1299175d174c0a2
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-14 16:22:24 -0400

    react redux application - cake store

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CakeContainer from "./components/CakeContainer";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
