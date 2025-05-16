5c1719146503df7bb6d0351a8acbc9f8addf1dee
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-16 15:57:03 -0400

    logger middleware

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+// keeps track of state chanegs
+import logger from "redux-logger";
 
 // store can have only one reducer
 // as there are multiple reducers - combined them into one single rootReducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
