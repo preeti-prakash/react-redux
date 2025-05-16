56792bdf969c3d4bff8b9258a7bc9a4043ce7e67
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-16 15:44:53 -0400

    combine reducers

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
 import rootReducer from "./rootReducer";
 
+// store can have only one reducer
+// as there are multiple reducers - combined them into one single rootReducer
 const store = createStore(rootReducer);
 
 export default store;
