ae4bc3994d83647270524449d9c4c5542ad9c330
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-16 16:10:15 -0400

    devTools extension

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
 // keeps track of state chanegs
 import logger from "redux-logger";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // store can have only one reducer
 // as there are multiple reducers - combined them into one single rootReducer
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 export default store;
