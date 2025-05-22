1fa194381a2d7bd604f478ebd8e4cec642812ac0
preeti-prakash <preetiprakashkandregula@gmail.com>
2025-05-22 13:24:12 -0400

    list of users get method using async

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,14 +1,13 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootReducer";
-// keeps track of state chanegs
+import { thunk } from "redux-thunk";
 import logger from "redux-logger";
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// store can have only one reducer
-// as there are multiple reducers - combined them into one single rootReducer
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(logger))
+  composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 export default store;
